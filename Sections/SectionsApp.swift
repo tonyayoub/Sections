61f5f72da5a5b96a8339faaff035acd298ca8899
Tony Ayoub <tonysameh@hotmail.com>
2023-02-21 04:37:03 +0100

    Initial Commit

---
+++ Sections/SectionsApp.swift
@@ -0,0 +1,20 @@
+//
+//  SectionsApp.swift
+//  Sections
+//
+//  Created by Tony Ayoub on 21-02-2023.
+//
+
+import SwiftUI
+
+@main
+struct SectionsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
